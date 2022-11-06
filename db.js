82b880c3201708d65abe5f6039f200457326d1c8
Swativerma4 <swati2727verma@gmail.com>
2022-11-06 11:27:57 +0530

    error2

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://swati04:swati123456@cluster0.7vcjcp7.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ const uri = "mongodb+srv://swati04:swati123456@cluster0.7vcjcp7.mongodb.net/?retryWrites=true&w=majority";
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const assert =require('assert');
-const url ='mongodb://localhost:27017';
+//const url ='mongodb://localhost:27017';
 const dbName= 'fruitsDB';
-const client=new MongoClient(url);
+//const client=new MongoClient(url);
 client.connect((err)=> {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
